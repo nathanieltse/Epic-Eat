f0afd30c9e5cb8ab4768a9df0837cda53146997a
nathanieltse <nathanieltse@hotmail.com>
2021-07-18 01:53:32 -0400

    added address search form

--- client/src/components/TopHeader/TopHeader.jsx
+++ client/src/components/TopHeader/TopHeader.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import locationIcon from '../../assets/icons/location.svg'
 import './TopHeader.scss'
 
 
 const TopHeader = ({location}) => {
+    const [expandform, setExpandform] = useState(true)
+    const [formInput, setFormInput] = useState("")
     return(
-        <div className="TopHeader">
-                <h1 className="TopHeader__text">{location}</h1>
-                <img className="TopHeader__icon" src={locationIcon} alt="location icon"/>
-        </div>
+        <section className="TopHeader">
+                <div className="TopHeader__header">
+                    <h1 className="TopHeader__header-text">{location}</h1>
+                    <img className="TopHeader__header-icon" src={locationIcon} alt="location icon"/>
+                </div>
+                {expandform &&
+                    <div className="TopHeader__body">
+                        <h1 className="TopHeader__body-title">Search</h1>
+                        <h4 className="TopHeader__body-subtitle">Add city name to get more accurate result.</h4>
+                        <form className="TopHeader__form">
+                            <input className="TopHeader__form-input" value={formInput}/>
+                            <button className="TopHeader__form-submit">Search</button>
+                        </form>
+                    </div>
+                }
+        </section>
     )
 }
 
